baa49b6aab66442800a61a4b0c35c9bf73734633
Adem Menk <adem_366@abv.bg>
2018-12-18 22:16:23 +0200

    experience and education section

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,36 @@
-var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    sass = require('gulp-ruby-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    webserver = require('gulp-webserver');
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var wait = require('gulp-wait');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('js', function() {
-  return gulp.src('builds/site/js/myscript.js')
-    .pipe(jshint('./.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'));
+gulp.task('scripts', function() {
+    return gulp.src('js/scripts.js')
+        .pipe(plumber(plumber({
+            errorHandler: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        })))
+        .pipe(uglify({
+            output: {
+                comments: '/^!/'
+            }
+        }))
+        .pipe(rename({extname: '.min.js'}))
+        .pipe(gulp.dest('js'));
 });
 
-gulp.task('sass', function () {
-    return sass('process/sass/style.scss', {
-      sourcemap: true,
-      style: 'expanded'
-    })
-    .on('error', function (err) {
-        console.error('Error!', err.message);
-    })
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('builds/site/css'));
+gulp.task('styles', function () {
+    return gulp.src('./scss/styles.scss')
+        .pipe(wait(250))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('builds/site/js/**/*', ['js']);
-  gulp.watch(['process/sass/**/*'], ['sass']);
+    gulp.watch('js/scripts.js', gulp.series('scripts'));
+    gulp.watch('scss/styles.scss', gulp.series('styles'));
 });
-
-gulp.task('webserver', function() {
-    gulp.src('builds/site/')
-        .pipe(webserver({
-            livereload: true,
-            open: true
-        }));
-});
-
-gulp.task('default', ['watch', 'sass','webserver']);
